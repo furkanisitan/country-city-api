cd17811ad3345e6ae117e03c1037288df8707610
Furkan Işıtan <furkanisitan96@gmail.com>
2021-09-16 19:35:39 +0300

    Add CountryLanguageId embeddable id

---
+++ src/main/java/com/furkanisitan/countrycityapi/entities/ids/CountryLanguageId.java
@@ -0,0 +1,42 @@
+package com.furkanisitan.countrycityapi.entities.ids;
+
+import com.furkanisitan.countrycityapi.entities.Country;
+import com.furkanisitan.countrycityapi.entities.Language;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Embeddable
+public class CountryLanguageId implements Serializable {
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+    @ManyToOne
+    @JoinColumn(name = "language_id")
+    private Language language;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CountryLanguageId id = (CountryLanguageId) o;
+        return Objects.equals(country, id.country) && Objects.equals(language, id.language);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(country, language);
+    }
+
+}
