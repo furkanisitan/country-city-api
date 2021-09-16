d254ef9d8c6f78f0020f7aa60ab52ef6cd60690a
Furkan Işıtan <furkanisitan96@gmail.com>
2021-09-16 03:51:02 +0300

    Create spring boot project

---
+++ src/main/java/com/furkanisitan/countrycityapi/CountryCityApiApplication.java
@@ -0,0 +1,13 @@
+package com.furkanisitan.countrycityapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryCityApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountryCityApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/furkanisitan/countrycityapi/CountryCityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furkanisitan.countrycityapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryCityApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
