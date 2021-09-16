ea1a931368dd370858564358bfd882deecb2a599
Furkan Işıtan <furkanisitan96@gmail.com>
2021-09-16 18:02:47 +0300

    Add Country entity

---
+++ src/main/java/com/furkanisitan/countrycityapi/entities/Country.java
@@ -0,0 +1,45 @@
+package com.furkanisitan.countrycityapi.entities;
+
+import com.furkanisitan.countrycityapi.core.entities.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.Column;
+import javax.persistence.Table;
+import java.util.Objects;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@javax.persistence.Entity
+@Table(name = "countries")
+@AttributeOverride(name = "id", column = @Column(name = "country_id", nullable = false))
+public class Country extends Entity<Long> {
+
+    @Column(unique = true, length = 5, nullable = false)
+    private String code;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    @Column(name = "life-expectancy", scale = 1)
+    private double lifeExpectancy;
+
+    //region equals & hashCode
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Country country = (Country) o;
+        return Objects.equals(code, country.code);
+    }
+
+    @Override
+    public int hashCode() {
+        return code == null ? getClass().hashCode() : code.hashCode();
+    }
+    //endregion
+
+}
