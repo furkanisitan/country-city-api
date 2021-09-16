179aab54960265f4b5b48a2207d9cad8d8a7450a
Furkan Işıtan <furkanisitan96@gmail.com>
2021-09-16 22:16:18 +0300

    Add CountryLanguage composite entity

---
+++ src/main/java/com/furkanisitan/countrycityapi/entities/CountryLanguage.java
@@ -0,0 +1,21 @@
+package com.furkanisitan.countrycityapi.entities;
+
+import com.furkanisitan.countrycityapi.core.entities.CompositeEntity;
+import com.furkanisitan.countrycityapi.entities.ids.CountryLanguageId;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@javax.persistence.Entity
+@Table(name = "country_languages")
+public class CountryLanguage extends CompositeEntity<CountryLanguageId> {
+
+    @Column(name = "is_official")
+    private boolean official;
+}
