5e5d0244e208b77834a6a88b50949c8e138e8886
furkanisitan <furkanisitan96@gmail.com>
2021-09-18 04:57:21 +0300

    Add method definitions to CityService interface

--- src/main/java/com/furkanisitan/countrycityapi/business/CityService.java
+++ src/main/java/com/furkanisitan/countrycityapi/business/CityService.java
@@ -1,5 +1,55 @@
 package com.furkanisitan.countrycityapi.business;
 
+import com.furkanisitan.countrycityapi.business.dtos.city.CityCreateDto;
+import com.furkanisitan.countrycityapi.business.dtos.city.CityDto;
+import com.furkanisitan.countrycityapi.business.dtos.city.CityUpdateDto;
+import com.furkanisitan.countrycityapi.core.exceptions.EntityNotExistsException;
+import com.furkanisitan.countrycityapi.core.exceptions.ForeignKeyConstraintViolationException;
+
+import java.util.List;
+
 public interface CityService {
+
+    /**
+     * Returns all cities by mapping them to {@link CityDto}.
+     *
+     * @return all cities by mapping them to {@link CityDto}.
+     */
+    List<CityDto> findAll();
+
+    /**
+     * Returns a {@link CityDto} by {@literal id}.
+     *
+     * @param id the primary key of the entity.
+     * @return a {@link CityDto} by {@literal id}.
+     */
+    CityDto findById(Long id);
+
+    /**
+     * Creates a new city.
+     *
+     * @param cityCreateDto the dto object required to create a new city.
+     * @return the added city by mapping it to {@link CityDto}.
+     * @throws ForeignKeyConstraintViolationException if country is not exists by {@link CityCreateDto#getCountryCode()}.
+     */
+    CityDto create(CityCreateDto cityCreateDto);
+
+    /**
+     * Updates the city.
+     *
+     * @param cityUpdateDto the dto object required to update the city.
+     * @throws EntityNotExistsException               if city is not exists.
+     * @throws ForeignKeyConstraintViolationException if country is not exists by {@link CityUpdateDto#getCountryCode()}.
+     */
+    void update(CityUpdateDto cityUpdateDto);
+
+    /**
+     * Deletes city by {@literal id}.
+     *
+     * @param id the primary key of the entity.
+     * @throws EntityNotExistsException if city is not exists by {@literal id}.
+     */
+    void deleteById(Long id);
+
 }
 
--- src/main/java/com/furkanisitan/countrycityapi/business/concretes/CityManager.java
+++ src/main/java/com/furkanisitan/countrycityapi/business/concretes/CityManager.java
@@ -1,10 +1,15 @@
 package com.furkanisitan.countrycityapi.business.concretes;
 
 import com.furkanisitan.countrycityapi.business.CityService;
+import com.furkanisitan.countrycityapi.business.dtos.city.CityCreateDto;
+import com.furkanisitan.countrycityapi.business.dtos.city.CityDto;
+import com.furkanisitan.countrycityapi.business.dtos.city.CityUpdateDto;
 import com.furkanisitan.countrycityapi.dataaccess.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Transactional(readOnly = true)
 @Service
@@ -15,5 +20,30 @@ public class CityManager implements CityService {
     @Autowired
     public CityManager(CityRepository repository) {
         this.repository = repository;
+    }
+
+    @Override
+    public List<CityDto> findAll() {
+        return null;
+    }
+
+    @Override
+    public CityDto findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public CityDto create(CityCreateDto cityCreateDto) {
+        return null;
+    }
+
+    @Override
+    public void update(CityUpdateDto cityUpdateDto) {
+
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
     }
 }
