c8f9d1ec53fd22ba437bd0afb4318ba117f2291f
furkanisitan <furkanisitan96@gmail.com>
2021-09-18 03:53:27 +0300

    Create api layer and add controllers

---
+++ src/main/java/com/furkanisitan/countrycityapi/api/controllers/CityController.java
@@ -0,0 +1,18 @@
+package com.furkanisitan.countrycityapi.api.controllers;
+
+import com.furkanisitan.countrycityapi.business.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/cities")
+public class CityController {
+
+    private final CityService cityService;
+
+    @Autowired
+    public CityController(CityService cityService) {
+        this.cityService = cityService;
+    }
+}
---
+++ src/main/java/com/furkanisitan/countrycityapi/api/controllers/CountryController.java
@@ -0,0 +1,18 @@
+package com.furkanisitan.countrycityapi.api.controllers;
+
+import com.furkanisitan.countrycityapi.business.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/cities")
+public class CountryController {
+
+    private final CountryService countryService;
+
+    @Autowired
+    public CountryController(CountryService countryService) {
+        this.countryService = countryService;
+    }
+}
