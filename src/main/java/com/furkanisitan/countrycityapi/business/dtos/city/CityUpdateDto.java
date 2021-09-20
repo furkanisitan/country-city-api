fc1e04bd4ec183cc7d2c65705a6af3f042b16e47
furkanisitan <furkanisitan96@gmail.com>
2021-09-20 06:20:56 +0300

    Add annotations to city dtos for validation
    - CityCreateDto
    - CityUpdateDto

--- src/main/java/com/furkanisitan/countrycityapi/business/dtos/city/CityUpdateDto.java
+++ src/main/java/com/furkanisitan/countrycityapi/business/dtos/city/CityUpdateDto.java
@@ -2,12 +2,23 @@ package com.furkanisitan.countrycityapi.business.dtos.city;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 @Data
 public class CityUpdateDto {
 
+    @NotNull
     private Long id;
+
+    @NotBlank
     private String name;
+
+    @Positive
     private long population;
+
+    @NotBlank
     private String countryCode;
 
 }
