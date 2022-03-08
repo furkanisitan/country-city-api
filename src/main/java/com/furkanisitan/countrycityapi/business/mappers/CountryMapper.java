943f4052bbdedf26d9d28cc888ad50851008632e
furkanisitan <furkanisitan96@gmail.com>
2022-03-08 15:18:31 +0300

    Update CountryMapper

--- src/main/java/com/furkanisitan/countrycityapi/business/mappers/CountryMapper.java
+++ src/main/java/com/furkanisitan/countrycityapi/business/mappers/CountryMapper.java
@@ -1,16 +1,17 @@
 package com.furkanisitan.countrycityapi.business.mappers;
 
 import com.furkanisitan.countrycityapi.model.entities.Country;
+import com.furkanisitan.countrycityapi.model.entities.CountryLanguage;
 import com.furkanisitan.countrycityapi.model.requests.CountryCreateRequest;
 import com.furkanisitan.countrycityapi.model.requests.CountryUpdateRequest;
 import com.furkanisitan.countrycityapi.model.responses.CountryCreateResponse;
+import com.furkanisitan.countrycityapi.model.responses.CountryLanguageResponse;
 import com.furkanisitan.countrycityapi.model.responses.CountryListResponse;
 import com.furkanisitan.countrycityapi.model.responses.CountryResponse;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
+import org.mapstruct.*;
 import org.mapstruct.factory.Mappers;
 
+import java.util.Collection;
 import java.util.List;
 
 @Mapper
@@ -20,7 +21,7 @@ public interface CountryMapper {
 
     List<CountryListResponse> toResponseList(List<Country> source);
 
-    @Mapping(target = "languages", source = "countryLanguages")
+    @Mapping(target = "languages", source = "countryLanguages", qualifiedByName = "toCountryLanguageResponseList")
     CountryResponse toResponse(Country source);
 
     CountryListResponse toListResponse(Country source);
@@ -28,8 +29,17 @@ public interface CountryMapper {
     @Mapping(target = "languages", source = "countryLanguages")
     CountryCreateResponse toCreateResponse(Country source);
 
+    @Mapping(target = "id", ignore = true)
     Country fromCreateRequest(CountryCreateRequest source);
 
     void updateFromUpdateRequest(CountryUpdateRequest source, @MappingTarget Country target);
+
+    @IterableMapping(qualifiedByName = "toCountryLanguageResponse")
+    @Named("toCountryLanguageResponseList")
+    List<CountryLanguageResponse> toCountryLanguageResponseList(Collection<CountryLanguage> source);
+
+    @Mapping(target = "languageId", source = "id.language.id")
+    @Named("toCountryLanguageResponse")
+    CountryLanguageResponse toCountryLanguageResponse(CountryLanguage source);
 
 }
