51940fa4be754c45dce49767567781fd9fe07ccd
furkanisitan <furkanisitan96@gmail.com>
2022-03-13 16:09:22 +0300

    Add ExampleUtils interface

---
+++ src/main/java/com/furkanisitan/core/utils/ExampleUtils.java
@@ -0,0 +1,38 @@
+package com.furkanisitan.core.utils;
+
+import com.furkanisitan.core.exceptions.CreateInstanceException;
+import com.furkanisitan.core.exceptions.NoSuchDeclaredFieldException;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+
+public interface ExampleUtils {
+
+    /**
+     * Create a new {@link Example} using the {@link ExampleMatcher.GenericPropertyMatchers#exact()}.
+     *
+     * @param clazz the {@link Class} instance of {@literal T}.
+     * @param name  the field name.
+     * @param value the field value.
+     * @param <T>   the type of probe.
+     * @param <V>   the type of value.
+     * @return a new {@link Example} instance.
+     * @throws CreateInstanceException if an error occurs while generating an object from {@literal clazz}.
+     */
+    static <T, V> Example<T> getExample(Class<T> clazz, String name, V value) {
+        try {
+            var probe = clazz.getDeclaredConstructor().newInstance();
+
+            var declaredField = GenericUtils.getField(clazz, name);
+            declaredField.set(probe, value);
+
+            var exampleMatcher = ExampleMatcher.matchingAny().withMatcher(name, ExampleMatcher.GenericPropertyMatchers.exact());
+
+            return Example.of(probe, exampleMatcher);
+        } catch (NoSuchDeclaredFieldException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new CreateInstanceException(e.getMessage());
+        }
+    }
+
+}
--- src/main/java/com/furkanisitan/core/validation/Validator.java
+++ src/main/java/com/furkanisitan/core/validation/Validator.java
@@ -1,13 +1,10 @@
 package com.furkanisitan.core.validation;
 
-import com.furkanisitan.core.exceptions.CreateInstanceException;
 import com.furkanisitan.core.exceptions.ForeignKeyConstraintException;
 import com.furkanisitan.core.exceptions.RecordNotFoundException;
 import com.furkanisitan.core.exceptions.UniqueConstraintException;
 import com.furkanisitan.core.model.Entity;
-import com.furkanisitan.core.utils.GenericUtils;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.ExampleMatcher;
+import com.furkanisitan.core.utils.ExampleUtils;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.util.Pair;
 
@@ -29,82 +26,67 @@ public abstract class Validator<T extends Entity<ID>, ID extends Serializable> {
     }
 
     /**
-     * @param field property path.
+     * @param name  the field name.
      * @param value the query value.
      * @param <V>   the type of value.
      * @return the {@link T} entity.
      * @throws RecordNotFoundException if entity not exists.
      */
-    public <V> T findBy(String field, V value) {
-        return repository.findOne(getExample(field, value))
-                .orElseThrow(() -> new RecordNotFoundException(clazz.getSimpleName(), Pair.of(field, value)));
+    public <V> T findBy(String name, V value) {
+        return repository.findOne(ExampleUtils.getExample(clazz, name, value))
+                .orElseThrow(() -> new RecordNotFoundException(clazz.getSimpleName(), Pair.of(name, value)));
     }
 
     /**
-     * @param field property path.
+     * @param name  the field name.
      * @param value the query value.
      * @param <V>   the type of value.
      * @return the {@link T} entity.
      * @throws RecordNotFoundException if entity not exists.
      */
-    public <V> T findForeignBy(String field, V value) {
-        return repository.findOne(getExample(field, value))
-                .orElseThrow(() -> new ForeignKeyConstraintException(field, value));
+    public <V> T findForeignBy(String name, V value) {
+        return repository.findOne(ExampleUtils.getExample(clazz, name, value))
+                .orElseThrow(() -> new ForeignKeyConstraintException(name, value));
     }
 
     /**
      * Checks an entity with the {@literal field} exists.
      *
-     * @param field property path.
+     * @param name  the field name.
      * @param value the query value.
      * @param <V>   the type of value.
      * @throws RecordNotFoundException if entity not exists.
      */
-    public <V> void existsBy(String field, V value) {
-        if (!repository.exists(getExample(field, value)))
-            throw new RecordNotFoundException(clazz.getSimpleName(), Pair.of(field, value));
+    public <V> void existsBy(String name, V value) {
+        if (!repository.exists(ExampleUtils.getExample(clazz, name, value)))
+            throw new RecordNotFoundException(clazz.getSimpleName(), Pair.of(name, value));
     }
 
     /**
      * Checks if {@literal field} is unique.
      *
-     * @param field property path.
+     * @param name  the field name.
      * @param value the query value.
      * @param <V>   the type of value.
      * @throws UniqueConstraintException if {@literal field} is not unique.
      */
-    public <V> void uniqueBy(String field, V value) {
-        if (repository.exists(getExample(field, value)))
-            throw new UniqueConstraintException(field, value);
+    public <V> void uniqueBy(String name, V value) {
+        if (repository.exists(ExampleUtils.getExample(clazz, name, value)))
+            throw new UniqueConstraintException(name, value);
     }
 
     /**
      * Checks if {@literal field} is unique for update.
      *
-     * @param field property path.
+     * @param name  the field name.
      * @param value the query value.
      * @param <V>   the type of value.
      * @throws UniqueConstraintException if {@literal field} is not unique for update.
      */
-    public <V> void uniqueBy(String field, V value, ID id) {
-        T entity = repository.findOne(getExample(field, value)).orElse(null);
+    public <V> void uniqueBy(String name, V value, ID id) {
+        T entity = repository.findOne(ExampleUtils.getExample(clazz, name, value)).orElse(null);
         if (entity != null && !Objects.equals(entity.getId(), id))
-            throw new UniqueConstraintException(field, value);
-    }
-
-    private <V> Example<T> getExample(String field, V value) {
-        try {
-            var probe = clazz.getDeclaredConstructor().newInstance();
-
-            var declaredField = GenericUtils.getField(clazz, field);
-            declaredField.set(probe, value);
-
-            var exampleMatcher = ExampleMatcher.matchingAny().withMatcher(field, ExampleMatcher.GenericPropertyMatchers.exact());
-
-            return Example.of(probe, exampleMatcher);
-        } catch (Exception e) {
-            throw new CreateInstanceException(e.getMessage());
-        }
+            throw new UniqueConstraintException(name, value);
     }
 
 }
