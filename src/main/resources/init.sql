ee692fa4d5d41cad9215077970206d10b3dca710
furkanisitan <furkanisitan96@gmail.com>
2022-03-13 04:58:22 +0300

    Update init.sql

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -5,22 +5,21 @@ VALUES (1, 'TR', 'Turkey', 78.6),
        (4, 'DE', 'Germany', 81.7);
 
 INSERT INTO languages (language_id, code, name)
-VALUES (1, 'ar', 'Arabic'),
-       (2, 'tr', 'Turkish'),
-       (3, 'ku', 'Kurdish'),
-       (4, 'en', 'English'),
-       (5, 'de', 'German');
+VALUES (1, 'tr', 'Turkish'),
+       (2, 'ku', 'Kurdish'),
+       (3, 'en', 'English'),
+       (4, 'de', 'German');
 
 INSERT INTO country_languages (country_id, language_id, is_official)
-VALUES (1, 2, true),
-       (1, 3, false),
-       (2, 4, true),
-       (3, 4, true),
-       (4, 5, true);
+VALUES (1, 1, true),
+       (1, 2, false),
+       (2, 3, true),
+       (3, 3, true),
+       (4, 4, true);
 
 INSERT INTO cities (city_id, country_id, name, population)
-VALUES (1, 1, 'Samsun', 1371274),
-       (2, 1, 'Ankara', 5747325),
+VALUES (1, 1, 'Ankara', 5747325),
+       (2, 1, 'Samsun', 1371274),
        (3, 2, 'Washington', 7766925),
        (4, 3, 'London', 8961989),
        (5, 4, 'Berlin', 3567000);
